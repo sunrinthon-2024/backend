09f4f756c175801370b5dd8b2dbeb56a5d2201c3
Yuchan Han <serltretu24@gmail.com>
2024-07-19 22:56:44 +0900

    feat: util get locally name

--- utils/string.py
+++ utils/string.py
@@ -6,16 +6,6 @@ def create_url(url: str, **kwargs) -> str:
 
 
 def get_country_locality_string(address_components: dict):
-    country = None
-    locality = None
-
-    for component in address_components:
-        if "country" in component["types"]:
-            country = component["long_name"]
-        elif "locality" in component["types"]:
-            locality = component["long_name"]
-
-    if country and locality:
-        return f"{locality}, {country}"
-    else:
-        return address_components["plus_code"]["compound_name"]
+    content = address_components["plus_code"]["compound_code"]
+    parts = content.split(",")
+    return ",".join(parts[1:]).strip()
