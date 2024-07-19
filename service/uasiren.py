99e22150a75a833c40fb407f41969e47c0565885
Yuchan Han <serltretu24@gmail.com>
2024-07-20 03:32:54 +0900

    fix: uasiren bug fixed

--- service/uasiren.py
+++ service/uasiren.py
@@ -10,8 +10,8 @@ from utils.request import BaseRequest
 load_dotenv(verbose=True)
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-ua_region_data_path = os.path.join(script_dir, '..', 'dataset', 'ua_regions.json')
-ua_region_lang_path = os.path.join(script_dir, '..', 'dataset', 'ua_region_match.json')
+ua_region_data_path = os.path.join(script_dir, "..", "dataset", "ua_regions.json")
+ua_region_lang_path = os.path.join(script_dir, "..", "dataset", "ua_region_match.json")
 
 
 class UASiren(BaseRequest):
@@ -27,7 +27,6 @@ class UASiren(BaseRequest):
         async with aiohttp.ClientSession() as session:
             self.uasiren_client = Client(session)
 
-
     def region_translate_ua(self, region_name: str) -> str:
         for region_en, region_ua in self.ua_region_lang.items():
             if (region_name in region_en) or (region_en in region_name):
@@ -41,11 +40,11 @@ class UASiren(BaseRequest):
     async def get_alerts(self, region_id: int):
         region_alert_datasets = await self.get(
             "https://map.ukrainealarm.com/api/data/getStatesHistory",
-            headers={"Authorization": "Bearer " + os.environ['UA_ALERT_TOKEN']},
+            headers={"Authorization": "Bearer " + os.environ["UA_ALERT_TOKEN"]},
         )
-        region_alert_datasets_json: list[dict] = json.loads(await region_alert_datasets.text())
+        region_alert_datasets_json: list[dict] = json.loads(
+            await region_alert_datasets.text()
+        )
         for each_region in region_alert_datasets_json:
             if each_region["regionId"] == str(region_id):
                 return each_region["alarms"]
-
-
