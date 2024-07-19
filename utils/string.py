6223c2780ae1cc6e6ef80efbd67384c4f9dbe9c2
Yuchan Han <serltretu24@gmail.com>
2024-07-19 23:07:18 +0900

    feat: calc distance util

--- utils/string.py
+++ utils/string.py
@@ -1,3 +1,4 @@
+import math
 from urllib.parse import urlencode
 
 
@@ -9,3 +10,23 @@ def get_country_locality_string(address_components: dict):
     content = address_components["plus_code"]["compound_code"]
     parts = content.split(",")
     return ",".join(parts[1:]).strip()
+
+
+def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float):
+    earthR = 6371.0
+
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = (
+        math.sin(dlat / 2) ** 2
+        + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    )
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = earthR * c
+    return distance
