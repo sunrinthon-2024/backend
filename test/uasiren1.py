ccb90d596ada41473bd551e5cfda268d05f0d39f
Yuchan Han <serltretu24@gmail.com>
2024-07-20 02:36:39 +0900

    test: ua siren

--- test/uasiren1.py
+++ test/uasiren1.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+
 import aiohttp
 from uasiren.client import Client
 
@@ -9,16 +11,18 @@ async def main():
 
         # All response formats are available here: https://api.ukrainealarm.com/swagger/index.html
         regions = await client.get_regions()
-        print("regions list", regions)
+        with open('../dataset/ua_regions.json', 'w') as file:
+            # Step 4: Use json.dump() to write the data
+            json.dump(regions, file, indent=2, ensure_ascii=False)
 
-        all_alerts = await client.get_alerts()
-        print("all alerts", all_alerts)
+        # all_alerts = await client.get_alerts()
+        # print("all alerts", all_alerts)
 
-        region_alerts = await client.get_alerts(16)
+        region_alerts = await client.get_alerts(4)
         print("alerts of region 16", region_alerts)
-
-        last_alert_index = await client.get_last_alert_index()
-        print("last alert index", last_alert_index)
+        #
+        # last_alert_index = await client.get_last_alert_index()
+        # print("last alert index", last_alert_index)
 
 
 asyncio.run(main())
